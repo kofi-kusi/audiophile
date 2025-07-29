543e1283ba66fb2e053129787fee9ba7640ab937
Kofi Kusi Appau <appaukusi05@gmail.com>
2025-07-29 12:16:59 +0000

    add tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 export default function App(){
     return(
-        <h1>Hey</h1>
+        <h1 className="text-red-600">Hey</h1>
     )
 }
