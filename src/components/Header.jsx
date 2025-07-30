9e4b49837d1b397544c45bb64d269f7711cb83d9
Kofi Kusi Appau <appaukusi05@gmail.com>
2025-07-30 13:11:09 +0000

    create header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,24 @@
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
+import hamburger from "../assets/shared/tablet/icon-hamburger.svg"
+import cartIcon from "../assets/shared/desktop/icon-cart.svg"
+
 
 export default function Header(){
     return(
-        <div className="max-w-9/10 mx-auto">
-            <div></div>
-            <div>
-                <Link to="/"><h1 className="text-white text-2xl font-bold">audiophile</h1></Link>
-            </div>
-            <div></div>
-        </div>
+        <header className="bg-black text-white border-b-1 border-gray py-8 px-6 flex justify-between items-center md:max-w-6xl mx-auto md:px-0">
+            <button className="md:mr-8 lg:hidden">
+                <img src={hamburger} alt="hamburger icon" />
+            </button>
+            <Link to="/" className="text-3xl font-black">audiophile</Link>
+            <nav className="hidden lg:flex justify-center items-center gap-5 font-bold">
+                <NavLink to="/" className="tracking-widest">HOME</NavLink>
+                <NavLink to="/" className="tracking-widesttracking-widest">HEADPHONES</NavLink>
+                <NavLink to="/" className="tracking-widest">SPEAKERS</NavLink>
+                <NavLink to="/" className="tracking-widest">EARPHONES</NavLink>  
+            </nav>
+            <button className="cursor-pointer md:ml-auto lg:ml-0">
+                <img src={cartIcon} alt="cart icon" />
+            </button>
+        </header>
     )
 }
