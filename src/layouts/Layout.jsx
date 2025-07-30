15a3d03667a3d3b746e4a1a62dd5c142da10414f
Kofi Kusi Appau <appaukusi05@gmail.com>
2025-07-30 14:04:08 +0000

    change: layout

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,22 +1,13 @@
-import Header from "../components/Header"
-import Footer from "../components/Footer"
-import { Outlet } from "react-router-dom"
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { Outlet } from "react-router-dom";
 
-export default function Layout(){
-    return(
-        <>
-            <header className="bg-black">
-                <Header />
-            </header>
-
-            <main>
-                <Outlet />
-            </main>
-            
-            <footer>
-                <Footer />
-            </footer>
-            
-        </>
-    )
+export default function Layout() {
+  return (
+    <div className="bg-black md:px-5">  
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 }
