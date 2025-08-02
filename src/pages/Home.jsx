86af7a5847f869aef8d1a02f62ff547795389be5
Kofi Kusi Appau <appaukusi05@gmail.com>
2025-08-02 21:10:47 +0000

    Refactor hero section medium screens
    Added an image for the tablet devices and modified the typography of the
    the text in the hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,28 @@
 import smImageHeader from "../assets/home/mobile/image-header.jpg";
+import mdImageHeader from "../assets/home/tablet/image-header.jpg"
 import Button from "../components/ui/Button";
 
 export default function Home() {
   return (
-    <div className="relative">
-      <img src={smImageHeader} alt="imageHeader" />
-      <div className="text-white absolute inset-0 flex flex-col justify-center items-center text-center md:max-w-6xl md:mx-auto px-8 md:px-0">
-        <p className="uppercase opacity-50 text-sm tracking-[10px] mb-[16px]">new product</p>
-        <h3 className="text-4xl leading-4xl tracking-[1.2px] uppercase font-bold mb-[24px]">XX99 Mark II HeadphoneS</h3>
-        <p className="opacity-75 text-[15px] font-medium mb-[28px]">Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
-        <Button />
+    <>
+      {/* Hero section */}
+      <div className="relative">
+        <img src={smImageHeader} className="md:hidden" alt="Header image" />
+        <img src={mdImageHeader} className="hidden md:block" alt="Header image" />
+        <div className="text-white absolute inset-0 flex flex-col justify-center items-center text-center px-8  md:max-w-[380px] md:mx-auto md:px-0">
+          <p className="uppercase opacity-50 text-sm tracking-[10px] mb-[16px] md:mb-[24px]">
+            new product
+          </p>
+          <h3 className="text-4xl leading-4xl tracking-[1.2px] uppercase font-bold mb-[24px]  md:text-[56px] md:leading-[58px] md:tracking-[2px]">
+            XX99 Mark II HeadphoneS
+          </h3>
+          <p className="opacity-75 text-[15px] font-medium mb-[28px] leading-[25px] md:mb-[40px]">
+            Experience natural, lifelike audio and exceptional build quality
+            made for the passionate music enthusiast.
+          </p>
+          <Button />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
