d42e3cb6fb677566d81b50f00c10b5ce2d21ab4d
Kofi Kusi Appau <appaukusi05@gmail.com>
2025-08-04 23:12:46 +0000

    Add category section
    I added a section to display all categories. Only responsive for
    mobile now

---
+++ src/components/ui/Category.jsx
@@ -0,0 +1,25 @@
+import { NavLink } from "react-router-dom";
+import imgCategoryThumbnailHeadphones from "../../assets/shared/desktop/image-category-thumbnail-headphones.png";
+import imgCategoryThumbnailEarphones from "../../assets/shared/desktop/image-category-thumbnail-earphones.png"
+import imgCategoryThumbnailSpeakers from "../../assets/shared/desktop/image-category-thumbnail-speakers.png"
+
+import CategoryThumbnail from "./CategoryThumbnail";
+
+export default function Category() {
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <CategoryThumbnail 
+        category="headphones"
+        imgURL={imgCategoryThumbnailHeadphones}
+      />
+      <CategoryThumbnail 
+        category="speakers"
+        imgURL={imgCategoryThumbnailSpeakers}
+      />
+      <CategoryThumbnail 
+        category="earphones"
+        imgURL={imgCategoryThumbnailEarphones}
+      />
+    </div>
+  );
+}
---
+++ src/components/ui/CategoryThumbnail.jsx
@@ -0,0 +1,31 @@
+import { NavLink } from "react-router-dom";
+import iconArrowRight from "../../assets/shared/desktop/icon-arrow-right.svg";
+
+
+export default function CategoryThumbnail({ category, imgURL}) {
+  return (
+    <>
+      <NavLink>
+        <div className="relative h-[217px] flex flex-col justify-end">
+          <img
+            className="absolute top-0 left-1/2 h-[150px] z-10 object-scale-down transform translate-x-[-50%]"
+            src={imgURL}
+            alt="headphones category thumbnail"
+          />
+
+          <div className="bg-light-gray flex flex-col items-center w-full h-[165px] rounded-lg pt-[88px] px-[108px] ">
+            <h2 className="uppercase text-[15px] font-bold leading-[24px] tracking-[1.3px]">
+              {category}
+            </h2>
+            <div className="flex gap-2 ">
+              <p className="opacity-50 uppercase text-[13px] font-bold tracking-wide">
+                shop
+              </p>
+              <img className="object-scale-down" src={iconArrowRight} alt="" />
+            </div>
+          </div>
+        </div>
+      </NavLink>
+    </>
+  );
+}
