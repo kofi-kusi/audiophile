4306fdca4839aa89c53ffe7cf484f0cceb86a22d
Kofi Kusi Appau <appaukusi05@gmail.com>
2025-08-04 23:24:38 +0000

    Update header menu style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,8 +7,8 @@ import logo from "../assets/shared/desktop/logo.svg"
 export default function Header(){
     return(
         <>
-        <header className="bg-black text-white     md:px-0">
-            <div className="py-8 px-6 flex justify-between items-center md:max-w-6xl md:mx-auto">
+        <header className="bg-black text-white px-6 ">
+            <div className="py-8 px-6 flex justify-between items-center md:max-w-6xl md:mx-auto md:px-0">
             <button className="md:mr-8 lg:hidden">
                 <img src={hamburger} alt="hamburger icon" />
             </button>
@@ -16,16 +16,16 @@ export default function Header(){
                 <img src={logo} alt="logo" />
             </Link>
             <nav className="hidden lg:flex justify-center items-center gap-5 font-bold text-sm">
-                <NavLink to="/" className="tracking-widest">HOME</NavLink>
-                <NavLink to="/" className="tracking-widest">HEADPHONES</NavLink>
-                <NavLink to="/" className="tracking-widest">SPEAKERS</NavLink>
-                <NavLink to="/" className="tracking-widest">EARPHONES</NavLink>  
+                <NavLink to="/" className="tracking-widest uppercase">home</NavLink>
+                <NavLink to="/" className="tracking-widest uppercase">headphones</NavLink>
+                <NavLink to="/" className="tracking-widest uppercase">speakers</NavLink>
+                <NavLink to="/" className="tracking-widest uppercase">earphones</NavLink>  
             </nav>
             <button className="cursor-pointer md:ml-auto lg:ml-0">
                 <img src={cartIcon} alt="cart icon" />
             </button>
             </div>
-            <hr className=" h-[2px] opacity-10 md:max-w-6xl mx-auto"/>
+            <hr className=" h-px opacity-10 w-full md:max-w-6xl md:mx-auto"/>
         </header>
         </>
     )
