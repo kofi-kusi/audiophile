dba8472f0bb528282456bee0373f0875d0eaba11
Kofi Kusi Appau <appaukusi05@gmail.com>
2025-08-05 16:14:41 +0000

    Add clsx

--- src/components/ui/Button.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function Button() {
-    return(
-        <button className="w-[160px] h-[48px] bg-burnt-orange text-white uppercase tracking-[1px] font-bold text-[13px] px-[30px] py-[15px]">
-            see product
-        </button>
-    )
-}
