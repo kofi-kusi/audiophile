7a7afe5035562fd72c64ec306304c7a39f64d333
Kofi Kusi Appau <appaukusi05@gmail.com>
2025-08-05 22:56:00 +0000

    Add bar at top

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,9 @@ import instagramIcon from "../assets/shared/desktop/icon-instagram.svg"
 export default function Footer(){
     return(
         <footer className="bg-black px-6">
-        <div className="grid grid-cols-1 justify-center items-center gap-10 text-white text-center py-10 px-6 md:max-w-6xl mx-auto md:auto-rows-auto md:grid-cols-2 md:text-left">
+        
+        <div className="relative grid grid-cols-1 justify-center items-center gap-10 text-white text-center py-10 px-6 md:max-w-6xl mx-auto md:auto-rows-auto md:grid-cols-2 md:text-left">
+            <div className="absolute top-0 left-1/2 transform -translate-x-1/2 md:translate-0 md:left-6 w-25 h-1 bg-burnt-orange"></div>
             <Link to="/" className="text-3xl font-black flex justify-center md:justify-start md:col-span-full lg:col-span-1 lg:col-end-2 lg:row-start-1 lg:row-end-2">
                 <img src={logo} alt="" />
             </Link>
