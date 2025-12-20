7002ecbda3c32bfdca74eec9d4fd8aecf4bdb879
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2025-12-20 14:56:26 +0000

    Create button variants

--- frontend/src/components/ui/Button.jsx
+++ frontend/src/components/ui/Button.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function Button({ text, bg }) {
+  const blackBtn =
+    "bg-[#000000] py-[15px] px-[30px] font-bold uppercase text-[#FFFFFF] text-[13px] tracking-[1px] w-[160px] cursor-pointer hover:bg-[#4C4C4C]";
+  const orangeBtn =
+    "bg-[#D87D4A] py-[15px] px-[30px] font-bold uppercase text-[#FFFFFF] text-[13px] tracking-[1px] w-[160px] hover:bg-[#FBAF85] cursor-pointer";
 
-  return (
-    <button className='bg-[#D87D4A] py-[15px] px-[30px] font-bold uppercase text-[#FFFFFF] text-[13px] tracking-[1px]'>
-        {text}
-    </button>
-  )
+  const getBtnVariant = bg === "black" ? blackBtn : orangeBtn;
+  return <button className={getBtnVariant}>{text}</button>;
 }
