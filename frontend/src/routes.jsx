3c7ba2eb8edd582d8d7a4a2c5834e51b17bf7ad3
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2025-12-22 13:13:40 +0000

    Setup route for categories

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,15 +1,21 @@
 import { createBrowserRouter } from "react-router";
-import Layout from "./components/Layout"
+import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
+import CategoryPage from "./pages/CategoryPage";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        Component: Layout,
-        children: [
-            {index: true, Component: HomePage}
-        ]
-    }
-])
+  {
+    path: "/",
+    Component: Layout,
+    children: [
+      { index: true, Component: HomePage },
+      {
+        path: ":category",
+        Component: CategoryPage,
+        children: [{ path: "earphones" }],
+      },
+    ],
+  },
+]);
 
 export default router;
