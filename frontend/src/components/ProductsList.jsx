cedf175460aa00881e17bcafab753a4eba5d6a1c
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2025-12-22 15:24:54 +0000

    Order products with new coming before old

--- frontend/src/components/ProductsList.jsx
+++ frontend/src/components/ProductsList.jsx
@@ -1,9 +1,13 @@
 import CategoryPageItem from "./CategoryPageItem";
 
 export default function ProductsList({ products }) {
+  const newProducts = products.filter((p) => p.new === true);
+  const oldProducts = products.filter((p) => !p.new);
+
+  const orderedProducts = [...newProducts, ...oldProducts];
   return (
     <div className="mt-16 md:mt-30 lg:mt-40 flex flex-col gap-30">
-      {products.map((product, index) => (
+      {orderedProducts.map((product, index) => (
         <CategoryPageItem product={product} index={index} />
       ))}
     </div>
