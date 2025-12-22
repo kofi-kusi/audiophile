278064aa58e2f9fc0b4eef2393b9454c4d5fcd80
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2025-12-22 13:51:20 +0000

    Display product data

--- frontend/src/components/CategoryPageItem.jsx
+++ frontend/src/components/CategoryPageItem.jsx
@@ -4,7 +4,7 @@ import Button from "./ui/Button";
 export default function CategoryPageItem({ product }) {
   return (
     <div className="flex flex-col gap-8 items-center lg:flex-row lg:gap-30">
-      <picture>
+      <picture className="lg:w-1/2">
         <source
           srcSet={product.categoryImage.desktop}
           media="(min-width: 992px)"
@@ -19,19 +19,17 @@ export default function CategoryPageItem({ product }) {
           className="rounded-lg"
         />
       </picture>
-      <div className="text-center flex flex-col gap-6 justify-between items-center md:max-w-[572px] lg:items-start lg:text-left">
+      <div className="text-center flex flex-col gap-6 justify-between items-center md:max-w-[480px] lg:items-start lg:text-left lg:w-1/2">
         {product.new && (
           <p className="text-[14px] uppercase leading-[19px] tracking-[10px] text-[#D87D4A]">
             new product
           </p>
         )}
         <h4 className="text-[28px] font-bold tracking-[2px] uppercase md:text-[40px] tracking-[1.5px] md:leading-[44px]">
-          XX99 Mark II Headphones
+          {product.name}
         </h4>
         <p className="font-normal leading-[25px] opacity-50 tracking-[0px]">
-          The new XX99 Mark II headphones is the pinnacle of pristine audio. It
-          redefines your premium headphone experience by reproducing the
-          balanced depth and precision of studio-quality sound.
+          {product.description}
         </p>
         <Button text="see product" />
       </div>
