276ee96d81245ecaf729942cc4de18b35026affb
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2025-12-24 13:57:16 +0000

    Ald alt attributes

--- frontend/src/components/ui/ProductGallery.jsx
+++ frontend/src/components/ui/ProductGallery.jsx
@@ -3,19 +3,19 @@ import React from "react";
 export default function ProductGallery({ gallery }) {
     console.log(gallery)
   return (
-    <div className="flex flex-col gap-4 md:flex-row md:gap-6 lg:gap-8">
+    <div className="flex flex-col gap-4 md:flex-row md:gap-6">
       <div className="flex flex-col justify-between gap-4">
         <div>
           <picture>
             <source
               srcSet={gallery.first.desktop}
               media="(min-width: 1000px)"
-              className=""
+              alt="Product gallery image"
             />
             <source
               srcSet={gallery.first.tablet}
               media="(min-width: 690px)"
-              className=""
+              alt="Product gallery image"
             />
             <img src={gallery.first.mobile} alt="" className="rounded-lg" />
           </picture>
@@ -25,12 +25,12 @@ export default function ProductGallery({ gallery }) {
             <source
               srcSet={gallery.second.desktop}
               media="(min-width: 1000px)"
-              className=""
+              alt="Product gallery image"
             />
             <source
               srcSet={gallery.second.tablet}
               media="(min-width: 690px)"
-              className=""
+              alt="Product gallery image"
             />
             <img src={gallery.second.mobile} alt="" className="rounded-lg" />
           </picture>
@@ -41,14 +41,14 @@ export default function ProductGallery({ gallery }) {
             <source
               srcSet={gallery.third.desktop}
               media="(min-width: 1000px)"
-              className=""
+              alt="Product gallery image"
             />
             <source
               srcSet={gallery.third.tablet}
               media="(min-width: 690px)"
-              className=""
+              alt="Product gallery image"
             />
-            <img src={gallery.third.mobile} alt="" className="rounded-lg" />
+            <img src={gallery.third.mobile} alt="Product gallery image" className="rounded-lg" />
           </picture>
       </div>
     </div>
