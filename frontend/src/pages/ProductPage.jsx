87712cf27995c06d270ebc78d1e59eaac051bdac
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2025-12-24 16:23:16 +0000

    Add Categories Thumbnails and BestGear

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -4,6 +4,8 @@ import GoBack from '../components/ui/GoBack';
 import { ProductsContext } from '../contexts/ProductsContext';
 import { useContext } from 'react';
 import { useParams } from 'react-router';
+import CategoriesThumbnail from '../components/CategoriesThumbnail';
+import BestGear from '../components/BestGear';
 
 export default function ProductPage() {
     const products = useContext(ProductsContext);
@@ -19,6 +21,8 @@ export default function ProductPage() {
     <main className='max-w-[1150px] mx-auto px-6 mt-4 md:mt-8 lg:mt-20'>
         <GoBack to={"/"+category} />
         <ProductDetails product={product}/>
+        <CategoriesThumbnail />
+        <BestGear />
     </main>
     </>
   )
