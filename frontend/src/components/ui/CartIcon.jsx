6e9a4a1e89b7b1c74591d34021bd1391166ca819
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2025-12-26 21:56:56 +0000

    Add CartModal

--- frontend/src/components/ui/CartIcon.jsx
+++ frontend/src/components/ui/CartIcon.jsx
@@ -1,39 +1,25 @@
-import React, { useState } from 'react'
-import cartIcon from "../../assets/shared/desktop/icon-cart.svg"
-import Modal from './Modal'
+import React, { useState } from "react";
+import cartIcon from "../../assets/shared/desktop/icon-cart.svg";
+import Modal from "./Modal";
+import CartModal from "../CartModal";
 
 export default function CartIcon() {
-    const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
-    function toggleModal() {
-      setIsOpen(prev => !prev)
-    }
-  
+  function toggleModal() {
+    setIsOpen((prev) => !prev);
+  }
+
   return (
     <>
-    <button onClick={toggleModal}>
+      <button onClick={toggleModal}>
         <img src={cartIcon} alt="Cart icon" />
-    </button>
-    <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
-      <div className="mx-auto max-w-[1150px] p-6 flex justify-end">
-
-      <div
-        className="bg-white rounded-lg p-6 max-w-md"
-        onClick={(e) => e.stopPropagation()} // prevent close on content click
-      >
-        <h2 className="text-xl font-bold">Modal Title</h2>
-        <p className="mt-2">This is the modal content jskjskjksjksjksj.</p>
-
-        <button
-          className="mt-4"
-          onClick={() => setIsOpen(false)}
-        >
-          Close
-        </button>
-      </div>
-      </div>
-        
+      </button>
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <div className="mx-auto max-w-[1150px] p-6 flex justify-end">
+          <CartModal />
+        </div>
       </Modal>
     </>
-  )
+  );
 }
