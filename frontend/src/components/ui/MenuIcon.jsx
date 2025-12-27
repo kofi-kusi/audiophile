4537d2809f3c77b30a3744fe5acadbb64d8ef58b
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2025-12-27 11:22:44 +0000

    Fix heiht of mobile nav menu on tablet

--- frontend/src/components/ui/MenuIcon.jsx
+++ frontend/src/components/ui/MenuIcon.jsx
@@ -16,8 +16,7 @@ export default function MenuIcon() {
       </button>
       <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
         <div
-          className="bg-white rounded-b-lg h-8/10 px-6 pb-8 w-full flex flex-col gap-8"
-          onClick={(e) => e.stopPropagation()} // prevent close on content click
+          className="bg-white rounded-b-lg h-8/10 md:h-fit px-6 pb-8 w-full flex flex-col gap-8"
         >
           <CategoriesThumbnail />
         </div>
