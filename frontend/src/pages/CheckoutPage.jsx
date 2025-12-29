b24452525219f40b5651d80c6927d89ed2c7fe7d
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2025-12-29 20:07:52 +0000

    Create summary section of checkout page

--- frontend/src/pages/CheckoutPage.jsx
+++ frontend/src/pages/CheckoutPage.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import GoBack from "../components/ui/GoBack";
+import { useCart } from "../hooks/useCart";
 
 export default function CheckoutPage() {
+  const { cart } = useCart();
+  const totalPrice = cart.reduce(
+    (sum, item) => sum + item.quantity * item.price,
+    0,
+  );
+  const shippingFee = 50;
+  const vat = 1079;
+
   return (
     <main className="max-w-[1150px] mx-auto px-6 mt-4 md:mt-8 lg:mt-20">
       <GoBack />
-      <div className="mt-6">
+      <div className="mt-6 mb-24 flex flex-col justify-between gap-8">
         <form
           action=""
           className="bg-[#FFFFFF] rounded-lg p-6 flex flex-col justify-between gap-8"
         >
-          <h4 className="text-[28px] uppercase leadin-[38px] tracking-[2px] font-bold">
+          <h4 className="text-[28px] uppercase leading-[38px] tracking-[2px] font-bold md:text-[32px] leading-[36px] tracking-px">
             Checkout
           </h4>
 
@@ -19,7 +28,7 @@ export default function CheckoutPage() {
               Billing details
             </legend>
 
-            <div className="flex flex-col justify-between gap-6 mt-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 justify-between gap-6 mt-4">
               <div className="flex flex-col justify-between gap-2">
                 <label htmlFor="name" className="text-[12px] font-bold">
                   Name
@@ -197,6 +206,73 @@ export default function CheckoutPage() {
             </div>
           </fieldset>
         </form>
+
+        <aside className="bg-[#FFFFFF] py-8 px-6 flex flex-col justify-between gap-8 rounded-lg">
+          <h6 className="text-[18px] uppercase leading-[24px] tracking-[1.3px] font-bold">
+            summary
+          </h6>
+          <div className="flex flex-col justify-between gap-6">
+            {cart.map((cartItem) => {
+              return (
+                <div className="flex justify-between">
+                  <div className="flex gap-4">
+                    <img
+                      src={cartItem.image}
+                      alt=""
+                      className="size-16 rounded-lg"
+                    />
+                    <div className="flex flex-col ">
+                      <p className="font-bold leading-[25px] tracking-0">
+                        {cartItem.name}
+                      </p>
+                      <p className="font-bold text-[14px] opacity-50 leading-[25px] tracking-0">
+                        $ {cartItem.price}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="opacity-50 leading-[25px] tracking-0 font-bold">
+                    x{cartItem.quantity}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="flex flex-col justify-between gap-2">
+            <div className="flex justify-between items-center">
+              <p className="uppercase font-medium leading-[25px] tracking-0 opacity-50">
+                Total
+              </p>
+              <p className="font-bold uppercase text-[18px] leading-0">
+                $ {totalPrice}
+              </p>
+            </div>
+            <div className="flex justify-between items-center">
+              <p className="uppercase font-medium leading-[25px] tracking-0 opacity-50">
+                Shipping
+              </p>
+              <p className="font-bold uppercase text-[18px] leading-0">
+                $ {shippingFee}
+              </p>
+            </div>
+            <div className="flex justify-between items-center">
+              <p className="uppercase font-medium leading-[25px] tracking-0 opacity-50">
+                Vat (Included)
+              </p>
+              <p className="font-bold uppercase text-[18px] leading-0">
+                $ {vat}
+              </p>
+            </div>
+          </div>
+          <div className="flex justify-between items-center">
+            <p className="uppercase font-medium leading-[25px] tracking-0 opacity-50">
+              Grand total
+            </p>
+            <p className="font-bold uppercase text-[18px] leading-0 text-[#D87D4A]">
+              $ {vat + shippingFee + totalPrice}
+            </p>
+          </div>
+          <button className="bg-[#D87D4A] text-[#FFFFFF] text-[13px] font-bold leading-px uppercase h-12 py-[15px] px-auto ">Continue & pay</button>
+        </aside>
       </div>
     </main>
   );
