17d1ba18c614982598679019dc07bb2815266584
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2026-01-12 14:40:55 +0000

    Setup scalar for api docs

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from scalar_fastapi import get_scalar_api_reference
 
-app = FastAPI()
+from .routers import user
 
-@app.get("/")
-def home():
-    return {"detail": "Hello there! This is Audiophile shop"}
+app = FastAPI(
+    title="Audiophile Shop",
+    docs_url=None,
+    redoc_url=None,
+)
+
+app.include_router(user.router)
+
+
+@app.get("/docs", include_in_schema=False)
+def get_scalar_docs():
+    return get_scalar_api_reference(
+        openapi_url=app.openapi_url,
+        title="Audiophile Shop",
+    )
