4a903cf35666fc937d0bfdc562d7cdefb127e29f
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2026-01-14 12:37:34 +0000

    Create a session generator

--- backend/app/database/session.py
+++ backend/app/database/session.py
@@ -1,16 +1,18 @@
-from sqlmodel import create_engine, Session, SQLModel
-from ..config import db_settings
-from .models import User
+from sqlmodel import Session, SQLModel, create_engine
 
+from ..config import db_settings
 
 # Create a database engine to connect with database
 engine = create_engine(
-    url= db_settings.POSTGRES_URL,
+    url=db_settings.POSTGRES_URL,
     echo=True,  # Log sql queries
 )
 
+
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
-    
 
 
+def get_session():
+    with Session(engine) as session:
+        yield session
