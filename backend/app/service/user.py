c91e983b60a5646e21efbec58462000b201e37cf
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2026-01-21 15:01:15 +0000

    Add method to verify users email

--- backend/app/service/user.py
+++ backend/app/service/user.py
@@ -1,6 +1,7 @@
 from datetime import timedelta
+from uuid import UUID
 
-from fastapi import HTTPException, status
+from fastapi import BackgroundTasks, HTTPException, status
 from pwdlib import PasswordHash
 from pydantic import EmailStr
 from sqlmodel import Session, select
@@ -8,15 +9,21 @@ from sqlmodel import Session, select
 from app.api.schemas.user import UserCreate
 from app.core.security import Token
 from app.database.models import User
-from app.utils import generate_access_token
+from app.service.notification import NotificationService
+from app.utils import (
+    decode_url_safe_token,
+    generate_access_token,
+    generate_url_safe_token,
+)
 
 password_hash = PasswordHash.recommended()
-ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+ACCESS_TOKEN_EXPIRE_MINUTES: int = 4
 
 
 class UserService:
-    def __init__(self, session: Session):
+    def __init__(self, session: Session, tasks: BackgroundTasks):
         self.session = session
+        self.notifications = NotificationService(tasks)
 
     def add(self, user_create: UserCreate):
         data: dict = user_create.model_dump()
@@ -29,10 +36,38 @@ class UserService:
         self.session.commit()
         self.session.refresh(user)
 
+        token = generate_url_safe_token({"id": str(user.id)})
+
+        self.notifications.send_email_with_template(
+            subject="Verify Your Account",
+            recipients=[user.email],
+            context={
+                "name": user.name,
+                "verification_url": f"http://localhost:8000/user/verify?token={token}",
+            },
+            template_name="mail_verify_email.html",
+        )
+
         return user
 
     def _get_user_by_email(self, email: str) -> User | None:
         return self.session.exec(select(User).where(User.email == email)).first()
+
+    def verify_email(self, token: str):
+        data = decode_url_safe_token(token)
+        if data is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid token",
+            )
+        id = UUID(data["id"])
+
+        user: User | None = self.session.get(User, id)
+        if user:
+            user.email_verified = True
+            self.session.add(user)
+            self.session.commit()
+            self.session.refresh(user)
 
     def token(self, email: EmailStr, password: str) -> Token:
         db_user = self._get_user_by_email(email)
