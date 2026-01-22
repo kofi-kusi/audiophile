e62335982e6137d6873fb7be5a36a43686d9e9a0
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2026-01-22 16:21:39 +0000

    Implement adding product functionality

--- backend/app/api/routers/product.py
+++ backend/app/api/routers/product.py
@@ -1,11 +1,20 @@
 from fastapi import APIRouter
-from ..schemas.tag import APITag
+
 from app.database.models import CategoryEnum
+
 from ..deps import ProductServiceDep
+from ..schemas.tag import APITag
+from ..schemas.product import ProductCreate, ProductPublic
 
-router = APIRouter(prefix="/{category}", tags=[APITag.PRODUCT])
+router = APIRouter(prefix="", tags=[APITag.PRODUCT])
 
-@router.get("/")
+
+@router.post(path="/", response_model=ProductPublic)
+def add_product(product: ProductCreate, service: ProductServiceDep):
+    return service.add_product(product)
+
+
+@router.get("/{category}")
 def get_category_products(category: CategoryEnum, service: ProductServiceDep):
     return service.get_caterory_products(category)
-   
+
--- backend/app/service/product.py
+++ backend/app/service/product.py
@@ -1,9 +1,21 @@
 from sqlmodel import Session
-from app.database.models import CategoryEnum
+
+from app.api.schemas.product import ProductCreate
+from app.database.models import CategoryEnum, Product
+
 
 class ProductService:
     def __init__(self, session: Session):
         self.session = session
+
+    def add_product(self, product: ProductCreate):
+        db_product = Product(**product.model_dump())
+
+        self.session.add(db_product)
+        self.session.commit()
+        self.session.refresh(db_product)
+
+        return db_product
 
     def get_caterory_products(self, category: CategoryEnum):
         return {
