59220bab39d67c0f82b6d6ca83bceec1f0ece8e5
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2026-01-23 15:19:28 +0000

    Add CORSMiddleware for local testing

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from scalar_fastapi import get_scalar_api_reference
 
 from .api.main import master_router
@@ -7,6 +8,17 @@ app = FastAPI(
     title="Audiophile Shop",
     docs_url=None,
     redoc_url=None,
+)
+
+
+origins = ["http://localhost:5173"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
